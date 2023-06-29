a103788ddc99a33183ca679ae280dd5f556ac12b
KutayUnal <119403349+KutayUnal@users.noreply.github.com>
2023-06-30 00:18:56 +0300

    E_ticaret_Uygulamasmaster6
    E_ticaretUygulamasmaster6

---
+++ E_ticaret_Uygulaması/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using E_ticaret_Uygulaması.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_ticaret_Uygulaması.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> UserManager;
+        private UserManager<ApplicationUser> userManager;
+        private RoleManager<ApplicationRole> RoleManager;
+
+        public AccountController()
+        {
+           var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            userManager = new UserManager<ApplicationUser>(userStore);
+            var roleStore=new RoleStore<ApplicationRole>(new IdentityDataContext());
+            RoleManager= new RoleManager<ApplicationRole>(roleStore);
+        }
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ E_ticaret_Uygulaması/Entity/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using E_ticaret_Uygulaması.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace E_ticaret_Uygulaması.Entity
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("dataConntection")
+        {
+            Database.SetInitializer(new DataInitializer());
+            //bu initializeri devreye sokuyoruz
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
